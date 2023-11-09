d193bcd341b035f0578db703016b0b519c3f234c
Muslimbek <muslimbeksalimov0102@gmail.com>
2023-11-09 20:17:47 +0500

    js

---
+++ js/main.js
@@ -0,0 +1,52 @@
+var Name = String(prompt("Ismingiz"));
+console.log(Name)
+
+var Surname = String(prompt("Familiya"));
+console.log(Surname)
+
+var Otaism = String(prompt("Otangizni ismi"));
+console.log(Otaism)
+
+var Raqam = Number(prompt("Telefon raqamingiz"));
+console.log(Raqam)
+
+var Ish = String(prompt("Nima Ish qilasiz"));
+console.log(Ish)
+
+var Qiziqish = String(prompt("Qiziqishlaringiz"));
+console.log(Qiziqish)
+
+var Hobby = String(prompt("Hobbyingiz"));
+console.log(Hobby)
+
+var Taom = String(prompt("Yaxshi ko'rgan ta'omingiz"));
+console.log(Taom)
+
+var Sport = String(prompt("Sevimli sporingiz"));
+console.log(Sport)
+
+var Nima = String(prompt("Nima sababdan buni yozyapsiz"));
+console.log(Nima)
+
+var Qanday = String(prompt("Qanday kompyuter ishlatasiz"));
+console.log(Qanday)
+
+var Javob = [
+    "Name:"+Name+"\n"+
+    "Surname:"+Surname+"\n"+
+    "Otaism:"+Otaism+"\n"+
+    "Raqam:"+Raqam+"/n"+
+    "Ish:"+Ish+"\n"+
+    "Qiziqish:"+Qiziqish+"\n"+
+    "Hobby:"+Hobby+"\n"+
+    "Taom:"+Taom+"\n"+
+    "Sport:"+Sport+"\n"+
+    "Nima:"+Nima+"\n"+
+    "Qanday"+Qanday+"\n"
+];
+
+alert(Javob);
+
+
+
+
